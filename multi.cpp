90a2fcb4c3dc3b6c240fd5c2456778faf882b696
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-02-25 13:44:17 +0100

    sorting app

--- multi.cpp
+++ multi.cpp
@@ -1,76 +1,56 @@
 #include <iostream>
-#include <thread>
 #include <vector>
+#include <thread>
 #include <algorithm>
 
-void quicksort(std::vector<int>& arr, int left, int right) {
-    int i = left, j = right;
-    int pivot = arr[(left + right) / 2];
-    
-    while (i <= j) {
-        while (arr[i] < pivot) {
-            i++;
-        }
-        while (arr[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            std::swap(arr[i], arr[j]);
-            i++;
-            j--;
-        }
-    }
+using namespace std;
 
-    if (left < j) {
-        quicksort(arr, left, j);
+// Function to partition the list
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
     }
-    if (i < right) {
-        quicksort(arr, i, right);
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// Function to perform quicksort on a sub-list
+void quicksort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
     }
 }
 
-void parallel_quicksort(std::vector<int>& arr, int left, int right, int depth) {
-    if (depth <= 0 || left >= right) {
-        quicksort(arr, left, right);
+// Function to perform multithreaded quicksort on a sub-list
+void multithreaded_quicksort(vector<int>& arr, int low, int high, int depth) {
+    if (depth >= 2 || low >= high) {
+        quicksort(arr, low, high);
         return;
     }
-
-    int i = left, j = right;
-    int pivot = arr[(left + right) / 2];
-
-    while (i <= j) {
-        while (arr[i] < pivot) {
-            i++;
-        }
-        while (arr[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            std::swap(arr[i], arr[j]);
-            i++;
-            j--;
-        }
-    }
-
-    std::thread left_thread(parallel_quicksort, std::ref(arr), left, j, depth - 1);
-    std::thread right_thread(parallel_quicksort, std::ref(arr), i, right, depth - 1);
-
+    int pi = partition(arr, low, high);
+    thread left_thread(multithreaded_quicksort, ref(arr), low, pi - 1, depth + 1);
+    thread right_thread(multithreaded_quicksort, ref(arr), pi + 1, high, depth + 1);
     left_thread.join();
     right_thread.join();
 }
 
-int main() {
-    std::vector<int> arr = {5, 3, 2, 7, 8, 1, 6, 4};
-    int num_threads = 2;
-    int depth = std::log2(num_threads);
-
-    parallel_quicksort(arr, 0, arr.size() - 1, depth);
-
-    for (int i = 0; i < arr.size(); i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
+// Function to perform the multithreaded quicksort on the entire list
+void parallel_quicksort(vector<int>& arr) {
+    multithreaded_quicksort(arr, 0, arr.size() - 1, 0);
 }
 
+int main() {
+    vector<int> arr = {3, 7, 1, 4, 9, 2, 6, 5, 8};
+    parallel_quicksort(arr);
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
